31ef1ad56b42e7767a7c5a2548d090cae3dc4764
leonardoolia <leonardooliaa@gmail.com>
2023-12-18 15:42:20 +0100

    calculated and added discounts

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,7 @@ let message = 'Il prezzo del tuo biglietto è: ';
 console.log(message);
 
 // 3. Chiediamo all'utente i chilometri da percorrere
-const userKm = prompt('Quanti kilometri vuoi percorrere?');
+const userKm = parseInt(prompt('Quanti kilometri vuoi percorrere?'));
 console.log(userKm);
 
 // 4. Creiamo una variabile per determinare il prezzo del biglietto
@@ -15,5 +15,30 @@ const ticketCost = userKm * 0.21;
 console.log(ticketCost);
 
 // 5. Chiediamo l'età
-const userAge = prompt('Quanti anni hai?');
+const userAge = parseInt(prompt('Quanti anni hai?'));
 console.log(userAge);
+
+// 6. Verifichiamo se l'utente è minorenne o over 65 e applichiamo sconti
+let minorDiscount;
+let overDiscount;
+
+let finalTicketCost
+
+if (userAge < 18) {
+    minorDiscount = ticketCost * 20 / 100;
+    finalTicketCost = ticketCost - minorDiscount;
+};
+
+if (userAge > 65) {
+    overDiscount = ticketCost * 40 / 100;
+    finalTicketCost = ticketCost - overDiscount;
+};
+
+console.log(finalTicketCost.toFixed(2));
+
+// 7. Scriviamo prezzo finale all'utente
+
+
+
+
+
