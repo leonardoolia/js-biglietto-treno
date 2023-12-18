7e34349869c1b1cdbf03aae033874c692603e17b
leonardoolia <leonardooliaa@gmail.com>
2023-12-18 15:11:30 +0100

    got html element and created message

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,9 @@
-console.log('JS OK')
+// 1. Recuperiamo l'elemento HTML
+const userTicketCost = document.getElementById('userTicketCost');
+console.log(userTicketCost);
+
+// 2. Prepariamo il messaggio finale per l'utente
+let message = 'Il prezzo del tuo biglietto è: ';
+console.log(message);
+
+
